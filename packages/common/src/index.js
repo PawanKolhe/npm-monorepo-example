7794c60260f81ed444fe2774074f23df58c3b32f
Pawan Kolhe <pawan@getcerta.com>
2025-01-16 23:02:07 +0530

    update util

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -1,23 +1,22 @@
 /**
- * This is a phantom dependency because it is not listed 
+ * This is a phantom dependency because it is not listed
  * in the package.json of this package.
- * 
- * This `date-fns` is being used from root `node_modules` 
+ *
+ * This `date-fns` is being used from root `node_modules`
  * directory which contains `date-fns` v1, a transitive
- * dependency of `cypress` package. 
- * 
+ * dependency of `cypress` package.
+ *
  * This is bad because the version of `date-fns` being used
  * here is currently dependent on `cypress` package.
- * 
+ *
  * Removing `cypress` from root `package.json` will change
  * the version of `date-fns` being used here to v2.
  */
 import { format } from "date-fns";
 
+// date-fns v1 format
+export const DATE_FORMAT = "DD/MM/YYYY";
+
 export const formatDate = (date) => {
-  return format(
-    date,
-    // date-fns v1 format
-    "DD/MM/YYYY"
-  );
+  return format(date, DATE_FORMAT);
 };
